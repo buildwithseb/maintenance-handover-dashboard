18d86aecc64bb264689f84f2fa633f4945cd421d
Sebastien CHAMPEAU <sebi72@live.fr>
2026-04-26 09:36:53 +0200

    add session to login page

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,20 +1,54 @@
 const { getDb } = require('../config/db');
 const bcrypt = require('bcrypt');
 
-exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;
-    req.session.user = {
-        email: req.body.email
-    };
-    res.status(200).json({ message: "Login successfull" });
+exports.postLogin = async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
 
+    const db = getDb();
+    const user = await db.collection('user').findOne({ email: email });
+    if (!user) {
+        return res.json({ message: "Email does not exist" })
+    }
+
+    const result = await bcrypt.compare(password, user.password);
+
+    if (result) {
+        req.session.isLoggedIn = true;
+        req.session.user = {
+            id: user._id.toString(),
+            email: email
+        };
+
+        return req.session.save(err => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({ message: "session save failed" });
+            }
+            res.status(200).json({ message: "User id logged in" });
+
+        });
+    }
+
+    res.json({ message: "Wrong password" });
 }
+
+
+
+
+
+
+
+
+
+
+
 
 exports.postSignUp = async (req, res, next) => {
 
     const email = req.body.email;
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10) ;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
     // const confirmPassword = req.body.confirmPassword;
 
     const db = getDb();
