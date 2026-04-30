0387a1d0c635826f908c2c9488b4493964f5a864
Sebastien CHAMPEAU <sebi72@live.fr>
2026-04-30 08:10:17 +0200

    delete auth.js file

--- frontend/src/services/auth.js
+++
@@ -1,8 +0,0 @@
-import ApiService from "./apiService.js";
-
-export default class AuthService {
-
-    static async postAuth(data, path) {
-         return await ApiService.postData(data, path);
-    }
-}
