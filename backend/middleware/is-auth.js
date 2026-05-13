97d486d6e4137a21b68fa685e00db7a6233a8689
Sebastien CHAMPEAU <sebi72@live.fr>
2026-05-13 07:49:58 +0200

    make is-auth middleware return 401 status

--- backend/middleware/is-auth.js
+++ backend/middleware/is-auth.js
@@ -1,6 +1,7 @@
 module.exports = (req, res, next) => {
     if (!req.session.isLoggedIn) {
-        return res.json({ isLoggedIn: "User not logged in" })
+        return res.status(401).json({ 
+            message:"Not authenticated" })
     }
     next()
 }
